2b1f3ddc9c960342091af2d15374d9ea99205bb6
ShourovHasan <shourovhasan@gmail.com>
2022-11-08 10:48:11 +0600

    mongodb added

--- index.js
+++ index.js
@@ -11,12 +11,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const userName = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${userName}:${password}@cluster0.qwlqnnv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 const run = async () => {
     try { 
         
 
-
+        
     }
     finally {
 
