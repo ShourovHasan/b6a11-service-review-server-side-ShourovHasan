9c26b97cce24ad0fcdd6dad7ab6a40b57a01c83e
ShourovHasan <shourovhasan@gmail.com>
2022-11-10 17:46:30 +0600

    My reviews delete part added

--- index.js
+++ index.js
@@ -140,7 +140,13 @@ const run = async () => {
             console.log(result);
             res.send(result);
         })
-        
+        // delete 
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
         
     }
     finally {
